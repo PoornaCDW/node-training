a75a2c16b72794bc281fc7ef9a87bff8d364be8b
PoornaCDW <125631705+PoornaCDW@users.noreply.github.com>
2023-03-29 22:37:02 +0530

    Optimizing the code.

--- exercise1/index.js
+++ exercise1/index.js
@@ -1,36 +1,16 @@
-const fileSystem = require("fs");
+const { readFileSync, writeFileSync } = require("fs");
 
-const colorPalette = JSON.parse(fileSystem.readFileSync("./color_ palette.json", "UTF-8", (error) => {
-    if(error)
-        console.log("Error");
-    else
-        console.log("File read succesfully!");
-}));
-
-let uniqueRandomNumbers = [];
-while(uniqueRandomNumbers.length < 5) {
-    let randNum = Math.floor(Math.random() * 44);
-    if(uniqueRandomNumbers.indexOf(randNum) === -1)
-        uniqueRandomNumbers.push(randNum);
-}
+const colorPalette = JSON.parse(readFileSync("./color_ palette.json", "UTF-8"));
 
 let randomColors = [];
-for(let randomColor of uniqueRandomNumbers) {
-    randomColors.push(colorPalette[randomColor]);
+while(randomColors.length < 5) {
+    let randNum = Math.floor(Math.random() * 44);
+    if(randomColors.indexOf(randNum) === -1)
+        randomColors.push(colorPalette[randNum]);
 }
 
-fileSystem.writeFileSync("random_generated_color_palette.json", JSON.stringify(randomColors), "UTF-8", (error) => {
-    if(error)
-        console.log("Error");
-    else
-        console.log("File written succesfully!");
-});
+writeFileSync("random_generated_color_palette.json", JSON.stringify(randomColors), "UTF-8");
 
-const randomGeneratedColorPalette = JSON.parse(fileSystem.readFileSync("random_generated_color_palette.json", "utf-8", (error) => {
-    if(error)
-        console.log("Error");
-    else
-        console.log("File read succesfully!");
-}));
+const randomGeneratedColorPalette = JSON.parse(readFileSync("random_generated_color_palette.json", "utf-8"));
 
 console.log(randomGeneratedColorPalette);
